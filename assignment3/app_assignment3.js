6151471083bf16e9d325a8eb2526e43a516c414d
israel_wong <israel_wong>
2016-10-17 15:37:07 +0800

    20161017 1537 Assignment 3

--- assignment3/app_assignment3.js
+++ assignment3/app_assignment3.js
@@ -3,42 +3,82 @@
 
 angular.module('NarrowItDownApp', [])
 .controller('NarrowItDownController', NarrowItDownController)
-.service('MenuSearchService', MenuSearchService);
+.service('MenuSearchService', MenuSearchService)
+.directive('foundItems', FoundItemsDirective);
 
 NarrowItDownController.$inject = ['$scope', 'MenuSearchService'];
 function NarrowItDownController($scope, MenuSearchService) {
 	var narrowItDownController = this;
+//	narrowItDownController.foundItems = [];
 	$scope.searchTerm = "";
 
 	narrowItDownController.getMatchedMenuItems = function() {
 		console.log("NarrowItDownController.getMatchedMenuItems called...");
 
-		var promise = MenuSearchService.getMatchedMenuItems($scope.searchTerm);
+		narrowItDownController.foundItems = MenuSearchService.getMatchedMenuItems($scope.searchTerm);
+//		console.log("Found items in NarrowItDownController: " + narrowItDownController.foundItems);
 
-		promise.then(function (response) {
-			narrowItDownController.temp = response.data
+		var promise = MenuSearchService.getMatchedMenuItems($scope.searchTerm)
+
+		promise
+		.then(function (response) {
+			narrowItDownController.foundItems = response;
+			console.log("Found items in NarrowItDownController: " + narrowItDownController.foundItems);
 		})
 		.catch(function (error) {
-			console.log("Something went wrong...");
+			console.log("Something went wrong in NarrowItDownController...");
 		});
 	}
 }
 
-MenuSearchService.$inject = ['$http'];
-function MenuSearchService($http) {
+MenuSearchService.$inject = ['$http', '$q'];
+function MenuSearchService($http, $q) {
 	var service = this;
 
 	service.getMatchedMenuItems = function(searchTerm) {
 		console.log("MenuSearchService.getMatchedMenuItems called...");
 		console.log("Searching for: " + searchTerm);
 
-		var response = $http({
+		var foundItems = [];
+
+		var deferred = $q.defer();
+
+		$http({
 			method: "GET",
 			url: ("https://davids-restaurant.herokuapp.com/menu_items.json")
+		})
+		.then(function (result) {
+			foundItems = result.data.menu_items;
+			
+			// TO DO filter the result list
+			// 
+			
+			console.log("Found items in MenuSearchService: " + foundItems);
+			deferred.resolve(foundItems);
+		})
+		.catch(function (error) {
+			console.log("Something went wrong in MenuSearchService...");
+			deferred.reject(error);
 		});
 
-		return response
+		foundItems = deferred.promise;
+
+		return $q.when(foundItems);
+		
+//		return response;
 	}
+}
+
+function FoundItemsDirective() {
+	var ddo = {
+		templateUrl: 'foundItems.html',
+		scope: {
+			items: '<',
+			onRemove: '&'
+		}
+	};
+
+	return ddo;
 }
 
 })();
