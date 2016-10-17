b87712bd5441da3a86a6905221b1553b707ff1a5
israel_wong <israel_wong>
2016-10-17 18:00:04 +0800

    20161017 1537 Assignment 3

--- assignment3/app_assignment3.js
+++ assignment3/app_assignment3.js
@@ -9,7 +9,7 @@ angular.module('NarrowItDownApp', [])
 NarrowItDownController.$inject = ['$scope', 'MenuSearchService'];
 function NarrowItDownController($scope, MenuSearchService) {
 	var narrowItDownController = this;
-//	narrowItDownController.foundItems = [];
+	narrowItDownController.foundItems = [];
 	$scope.searchTerm = "";
 
 	narrowItDownController.getMatchedMenuItems = function() {
@@ -23,11 +23,16 @@ function NarrowItDownController($scope, MenuSearchService) {
 		promise
 		.then(function (response) {
 			narrowItDownController.foundItems = response;
-			console.log("Found items in NarrowItDownController: " + narrowItDownController.foundItems);
+			console.log("Found " + narrowItDownController.foundItems.length + " items matching the criteria in NarrowItDownController.");
 		})
 		.catch(function (error) {
 			console.log("Something went wrong in NarrowItDownController...");
 		});
+	}
+
+	narrowItDownController.removeItem = function(itemIndex) {
+		console.log("NarrowItDownController.onRemove called...");
+		narrowItDownController.foundItems.splice(itemIndex, 1);
 	}
 }
 
@@ -39,7 +44,8 @@ function MenuSearchService($http, $q) {
 		console.log("MenuSearchService.getMatchedMenuItems called...");
 		console.log("Searching for: " + searchTerm);
 
-		var foundItems = [];
+		var allItems = [];
+		var foundItems = undefined;
 
 		var deferred = $q.defer();
 
@@ -48,12 +54,19 @@ function MenuSearchService($http, $q) {
 			url: ("https://davids-restaurant.herokuapp.com/menu_items.json")
 		})
 		.then(function (result) {
-			foundItems = result.data.menu_items;
-			
+			allItems = result.data.menu_items;
+			foundItems = [];
+
 			// TO DO filter the result list
 			// 
-			
-			console.log("Found items in MenuSearchService: " + foundItems);
+			for (var i = 0; i < allItems.length; i++) {
+				if (allItems[i].description.toLowerCase().indexOf(searchTerm.toLowerCase()) > -1) {
+					console.log(allItems[i].description);
+					foundItems.push(allItems[i]);
+				}
+			}
+
+			console.log("Found " + foundItems.length + " items matching the criteria in MenuSearchService.");
 			deferred.resolve(foundItems);
 		})
 		.catch(function (error) {
