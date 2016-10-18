c3763aa4c141b7e59d1d4bab68a86cfc966a4553
israel_wong <israel_wong>
2016-10-19 00:06:45 +0800

    20161019 0006 Assignment 4

--- assignment4/src/menuapp/menudata.service.js
+++ assignment4/src/menuapp/menudata.service.js
@@ -4,66 +4,76 @@
 angular.module('data')
 .service('MenuDataService', MenuDataService);
 
-MenuDataService.$inject = ['$q', '$timeout']
-function MenuDataService($q, $timeout) {
+MenuDataService.$inject = ['$http', '$q']
+function MenuDataService($http, $q) {
   var service = this;
 
   // List of shopping items
-  var categories = [];
-  var items = [];
+//  var items = [];
 
   // MDEMDE Pre-populate the categories list (for testing purpose)
-  categories.push({"short_name": "A", "description": "DNLM 1st"});
-  categories.push({"short_name": "B", "description": "DNLM 2nd"});
+//  categories.push({"short_name": "A", "description": "DNLM 1st"});
+//  categories.push({"short_name": "B", "description": "DNLM 2nd"});
   
   // MDEMDE Pre-populate a no cookie list (for testing purpose)
-  items.push({
-    category: "A",
-    name: "Sugar",
-    quantity: "2 bags",
-    description: "Sugar used for baking delicious umm... baked goods."
-  });
-  items.push({
-    category: "B",
-    name: "flour",
-    quantity: "1 bags",
-    description: "High quality wheat flour. Mix it with water, sugar, 2 raw eggs."
-  });
-  items.push({
-    category: "A",
-    name: "Chocolate Chips",
-    quantity: "3 bags",
-    description: "Put these in the dough. No reason, really. Gotta store them somewhere!"
-  });
-
-  // Simulates call to server
-  // Returns a promise, NOT items array directly
-  service.getItems = function () {
-    var deferred = $q.defer();
-
-    // Wait 2 seconds before returning
-    $timeout(function () {
-      // deferred.reject(items);
-      deferred.resolve(items);
-    }, 800);
-
-    return deferred.promise;
-  };
+//  items.push({
+//    category: "A",
+//    name: "Sugar",
+//    quantity: "2 bags",
+//    description: "Sugar used for baking delicious umm... baked goods."
+//  });
+//  items.push({
+//    category: "B",
+//   name: "flour",
+//    quantity: "1 bags",
+//    description: "High quality wheat flour. Mix it with water, sugar, 2 raw eggs."
+//  });
+//  items.push({
+//    category: "A",
+//    name: "Chocolate Chips",
+//    quantity: "3 bags",
+//    description: "Put these in the dough. No reason, really. Gotta store them somewhere!"
+//  });
 
   service.getAllCategories = function() {
     console.log("Calling MenuDataService.getAllCategories()...");
     // TODO
     //
-    
-    return categories;
+    return service.callWebService("https://davids-restaurant.herokuapp.com/categories.json");
   };
 
   service.getItemsForCategory = function(categoryShortName) {
     console.log("Calling MenuDataService.getItemsForCategory(categoryShortName)..., categoryShortName = " + categoryShortName + "...");
     // TODO
     //
-    return items;
+    return service.callWebService("https://davids-restaurant.herokuapp.com/menu_items.json?category=" + categoryShortName);
   };
+
+  service.callWebService = function(url) {
+    console.log("Calling MenuDataService.callWebService()...");
+    var data = undefined;
+
+    var deferred = $q.defer();
+
+    $http({
+      method: "GET",
+      url: (url)
+    })
+    .then(function (result) {
+      console.log("Response received in MenuDataService.getAllCategories()...");
+      data = [];
+      data = result.data;
+      deferred.resolve(data);
+    })
+    .catch(function (error) {
+      console.log("Something went wrong in MenuDataService.getAllCategories()...");
+      deferred.reject(error);
+    });
+
+    data = deferred.promise;
+
+    return $q.when(data);
+  }
 }
 
 })();
